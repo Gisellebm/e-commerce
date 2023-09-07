c9cbd65da705aa6dd554f1b39df26953e0d63801
Gisellebm <gisellebrasil.cf@gmail.com>
2023-09-07 18:19:47 -0300

    projeto Finalizado

--- checkout.js
+++ checkout.js
@@ -1,10 +1,34 @@
-import { desenharProdutoNoCarrinhoSimples, lerLocalStorage } from "./src/utilidades"
+import { apagarDoLocalStorage, desenharProdutoNoCarrinhoSimples, lerLocalStorage, salvarLocalStorage } from "./src/utilidades"
 
 function desenharProdutosCheckout() {
-    const idsProdutoCarrinhoComQuantidade = lerLocalStorage('carrinho')
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage('carrinho') ?? {}
     for (const idProduto in idsProdutoCarrinhoComQuantidade) {
         desenharProdutoNoCarrinhoSimples(idProduto, 'container-produtos-checkout', idsProdutoCarrinhoComQuantidade[idProduto])
     }
 }
 
+function finalizarCompras(evento) {
+    evento.preventDefault()
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage('carrinho') ?? {}
+    if (Object.keys(idsProdutoCarrinhoComQuantidade).length === 0) {
+        return
+    }
+
+    const dataAtual = new Date()
+    const pedidoFeito = {
+        dataPedido: dataAtual,
+        pedido: idsProdutoCarrinhoComQuantidade
+    }
+
+    const historicoDePedidos = lerLocalStorage('historico-de-pedidos') ?? []
+    const historicoDePedidosAtualizado = [...historicoDePedidos, pedidoFeito]
+    
+    salvarLocalStorage('historico-de-pedidos', historicoDePedidosAtualizado)
+    apagarDoLocalStorage('carrinho')
+    
+    window.location.href = "./pedidos.html"
+}
+
 desenharProdutosCheckout()
+
+document.addEventListener('submit', (evento) => finalizarCompras(evento))
--- src/utilidades.js
+++ src/utilidades.js
@@ -73,13 +73,17 @@ export function lerLocalStorage(chave) {
   return JSON.parse(localStorage.getItem(chave))
 }
 
+export function apagarDoLocalStorage(chave) {
+  localStorage.removeItem(chave)
+}
+
 export function desenharProdutoNoCarrinhoSimples(idProduto, idContainerHtml, quantidadeProduto) {
   const produto = catalogo.find(p => p.id === idProduto)
 
   const containerProdutosCarrinho = document.getElementById(idContainerHtml)
 
   const elementoArticle = document.createElement("article")
-  const articleClasses = ["flex", "gap-4", "p-1", "relative", "bg-stone-200", "rounded-lg", "mb-2"]
+  const articleClasses = ["flex", "gap-4", "p-1", "relative", "bg-stone-200", "rounded-lg", "mb-2", "w-96"]
 
   for (const articleClass of articleClasses) {
       elementoArticle.classList.add(articleClass)
